9109478f240b788399b9f6777469646f9bd34114
Sarthak <sc2309>
2023-05-06 21:02:11 +0530

    Printing variable and data types PY

---
+++ src/basics/data_types.py
@@ -0,0 +1,13 @@
+x = 5 # this is an integer value
+print(x)
+y = "Parth"  # this is an string value or 'Parth'
+print(y)
+z = 3.1459   # this is an float value
+print(z)
+a = 4j  # this is an complex value
+print(a)
+b = ["pari","parth","dibbu","shivay"]
+print(b)
+c = ("nana ji","nani","mama","mami")
+print(c)
+
--- src/basics/variables.py
+++ src/basics/variables.py
@@ -1,2 +1,13 @@
-x = 5 # this is an int
+x = 5
 print(x)
+y = 'sarthak' # or "sarthak"
+print(y)
+z = float(3) # we can use int(),str()  here 3 = 3.0
+print(z)
+a = 2
+a = "changed data type"    # int to string
+print(a)
+b,c,d = 1,2,3
+print(b,c,d)
+e,f,g = "hello world"
+print(e,f,g)
