ef28da8c1b3d0a544030a0e21b8f0a31d8aec5cb
Sarthak <sc2309>
2023-05-07 13:54:15 +0530

    Printing variable and data types PY

--- src/basics/variables.py
+++ src/basics/variables.py
@@ -11,3 +11,14 @@ b, c, d = 1, 2, 3
 print(b, c, d)
 e, f, g = "hello"
 print(e, f, g)
+
+m = "monday"
+def localVar():
+    m = 7
+    print(m)
+print(m)
+
+def globalVar():
+    global n
+    n = 190
+print(n)
