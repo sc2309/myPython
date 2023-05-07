49ffabb4627fdfa4f93a942c87744257f1c53a4b
Sarthak <sc2309>
2023-05-07 16:34:35 +0530

    Printing variable and data types PY

---
+++ src/basics/NubersOperation.py
@@ -0,0 +1,25 @@
+import random
+x = 5j   #  this is a complex data type
+y = -5 + 5j    #   this is complex data type
+print(x, " ", y)
+
+#  conversion
+
+a = 2.903
+b = 45
+c = 5+6j
+
+d = int(a)
+e = float(b)
+f = complex(c)
+
+print(d, e, f)
+
+#    Note: You cannot convert complex numbers into another number type.
+
+#    Random Numbers
+
+print(random.randrange(100.103))
+
+
+#    s
