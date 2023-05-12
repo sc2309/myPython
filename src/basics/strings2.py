e1e3b2dd21b20894a05310be97ec316c1d3d73d7
Sarthak <sc2309>
2023-05-12 09:27:12 +0530

    Operations on strings file 2

---
+++ src/basics/strings2.py
@@ -0,0 +1,25 @@
+#       accessing a character in a string 
+a = 'i like coding'
+print(a[0], "\n")
+
+#       accessing every character in a string
+for x in a:
+    print(x)
+
+#   length of a string  imp
+
+print(len(a))
+
+#   checking a word or a character in a string
+
+print('coding' in a)
+
+#   checking a word or a character in a string using if else and not in
+
+if 'worse' not in a:
+    print('good statement')
+
+#    slicing technique
+
+print(a[7:13])
+print(a[-13:-7])
