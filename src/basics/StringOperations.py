9b1bb0c64ec452ce718666306d1ab08f095c0523
Sarthak <sc2309>
2023-05-12 09:26:23 +0530

    Operations on strings

---
+++ src/basics/StringOperations.py
@@ -0,0 +1,17 @@
+#   multiline string
+
+text = """I am sarthak chouksey
+i study in class 9th D
+i want to be a software engineer"""
+print(text, "\n")
+
+#   same can be done with ''' text '''
+
+#array and strings
+
+a = "hello"
+print(a[0])
+
+text = 'i am sarthak'
+for x in range(1, 12):
+    print(text[x], "\n")
