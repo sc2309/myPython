938b24e9cab013df35f3a73eed23f38af077ef77
Sarthak <sc2309>
2023-05-17 11:48:38 +0530

    data types in PY with inbuilt functions

--- src/basics/data_types.py
+++ src/basics/data_types.py
@@ -13,6 +13,16 @@ b.insert(2, "No One")
 print(b)
 b.remove("No One")
 print(b)
+temp = [2,5,7,3,9,8,4,0,1,6] #  to delete multiple values
+print(temp)
+del temp[1:8]
+print(temp)
+temp.extend([23,56,77,88,99,20])
+print(min(temp))
+print(max(temp))
+print(sum(temp))
+temp.sort()
+print(temp)
 c = ("Ram Prasad", "Ahilya", "Prince", "Nikita")    #    this is a tupple in ()
 print(c)
 d = {"name" : "John", "age" : 36}	  #   this is a dict by assigning multiple values in curly braces {}
