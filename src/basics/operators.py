2c1c6b85dc94629de9117ec65fa047fb7ed989c8
Sarthak <sc2309>
2023-05-23 21:50:37 +0530

    done some operations and modified files

---
+++ src/basics/operators.py
@@ -0,0 +1,16 @@
+x = 5 + 5
+y = 5 - 5
+z = 25 / 5
+a = 25 // 5
+b = 2
+b += 10   #  same can be done with -= *= /=
+c = True
+not c
+d = 10 % 2
+print(x,"\n",y,"\n",z,"\n",a,"\n",b,"\n",c,"\n",d)
+print(x == y)
+print(x != y)
+print(x <= y)
+print(x >= y)
+print(x == y and z == a)
+print(x == y or z == a)
