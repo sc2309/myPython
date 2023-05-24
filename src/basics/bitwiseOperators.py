9d29329a84c7e49e0a45ac87e51732a0538c2b47
Sarthak <sc2309>
2023-05-24 22:00:09 +0530

    learned bitwise operators

---
+++ src/basics/bitwiseOperators.py
@@ -0,0 +1,17 @@
+print(~12)         # this will print 13 as reverse of 12 in binary
+
+print(12 & 13)     # output 12 as in binary 12 and 13  eg.  0010011
+                   #                                        0100110
+                   #                                        -------
+                   #                                        0000010
+
+print(12 | 13)     # output 12 as in binary 12 and 13  eg.  0010011
+                   #                                        0100110
+                   #                                        -------
+                   #                                        0110111
+print(12 ^ 13)     # output 1 as in binary 12 and 13  eg.  0010011
+                   #                                        0100110
+                   #                                        -------
+                   #                                        0110101
+print(10 << 2)    # output is 40 as eg. 12 = 1010 and it gains 2 decimal now it is 101000 = 40
+print(10 >> 2)    # output is 2 as eg. 12 = 1010 it looses 2 decimal now it is 10 = 2
