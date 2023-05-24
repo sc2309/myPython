33ceca4be8ed408612d02b971169850a0d50d73a
Sarthak <sc2309>
2023-05-24 21:22:43 +0530

    learned about binary,hex,octal value

--- src/basics/binary_values.py
+++ src/basics/binary_values.py
@@ -1,6 +1,6 @@
-x = bin(250)   #    binary form 11111010
-y = hex(256)   #    hexagonal form 100
-z = oct(256)   #    octal form 400
+x = bin(250)   #    binary form 11111010.
+y = hex(256)   #    hexagonal form 100.
+z = oct(256)   #    octal form 400.
 print(x)
 print(y)
 print(z)
