8ad3e6c41f5be60003b2e6d342670a03b05a829e
Prince Shivhare <prince.shivhare@edg.com.au>
2023-05-25 10:13:13 +1000

    mapping the data with json.

--- FlaskWebApp1/JsonAPI/CallToAPI.py
+++ FlaskWebApp1/JsonAPI/CallToAPI.py
@@ -1,9 +1,10 @@
 import requests
+from MapData.MapData import User 
 
 headers = {'Accept': 'application/json'}
 user_data = input("Enter Data Number - ")
 api_url = 'https://jsonplaceholder.typicode.com/todos/'+user_data
 r = requests.get(api_url, headers=headers)
 getData = r.json()
-2
-print(f"Json Response is -  {getData}")
+User.id = getData["title"]
+print(User.id)
---
+++ FlaskWebApp1/JsonAPI/MapData/MapData.py
@@ -0,0 +1,5 @@
+class User():
+    userId : int
+    id: int
+    title: str
+    completed : bool
