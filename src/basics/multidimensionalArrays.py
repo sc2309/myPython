51a48ec921732b09d34d25acf2912871dda0ced4
Sarthak <sc2309>
2023-06-03 22:13:49 +0530

    learned different ways to create an array.

--- src/basics/multidimensionalArrays.py
+++ src/basics/multidimensionalArrays.py
@@ -1,4 +1,12 @@
 from numpy import *
-arr = array([1, 2, 3], [4, 5, 6])
+arr = array([1, 2, 3])
 print(arr)
 # need work with multidimensional array
+i = arange(0, 15, 2)
+print(i)
+
+j = ones(5, int)     # ones give 1and zeros gives 0 with array size
+print(j)
+
+k = linspace(0, 15, 6)
+print(k)
