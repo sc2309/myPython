6e2d1852d17b51ee238f5eec409d8dcec9807da2
Sarthak <sc2309>
2023-06-07 23:06:40 +0530

    revising Basics

---
+++ src/practise_basics/For_else_prac.py
@@ -0,0 +1,7 @@
+count = int(input())
+for i in range(count):
+    if count <= 2:
+        break
+    count += 1
+else:
+    print(count)
---
+++ src/practise_basics/MATH.py
@@ -0,0 +1,6 @@
+from math import *
+x = 10
+y = 5
+print(min(x, y))  #   same can be done with max()
+print(sqrt(x))
+print(factorial(x))
