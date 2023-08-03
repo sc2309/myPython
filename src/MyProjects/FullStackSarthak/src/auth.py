9fd7cb23732d76b3192e6bfd2f58f1f515ae5fe8
Sarthak <sc2309>
2023-08-03 12:43:31 +0530

    Added a bit of CSS

--- src/MyProjects/FullStackSarthak/src/auth.py
+++ src/MyProjects/FullStackSarthak/src/auth.py
@@ -4,17 +4,12 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/result', methods=['GET', 'POST'])
 def login():
-    x = int(request.form.get('name'))
-    if x >= 65 and x <= 80:
-        head = 'A'
-    elif x <= 64 and x >= 50:
-        head = 'B'
-    elif x <= 49 and x >= 35:
-        head = 'C'
-    elif x <= 34 and x >= 18:
-        head = 'D'
-    elif x <= 17 and x >= 0:
-        head = 'F'
-    else:
-        head = 'Invalid Marks'
+    M = int(request.form.get('math_marks'))
+    S = int(request.form.get('science_marks'))
+    EH = int(request.form.get('EngHin_marks'))
+    C = int(request.form.get('Comp_marks'))
+    S = int(request.form.get('SSC_marks'))
+    T = int(request.form.get('total_marks'))
+    sum = M + S + EH + C + S
+    head = sum/T * 100
     return render_template('result.html', headtype=head)
